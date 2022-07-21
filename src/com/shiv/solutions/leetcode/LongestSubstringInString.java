ca1d1cc5fdfa3b2ad8679266f9c2d503a6a07aac
Shivshankar Sahoo <sahoo.shivshankar@gmail.com>
2022-07-22 00:05:35 +0530

    Added javadoc

--- src/com/shiv/solutions/leetcode/LongestSubstringInString.java
+++ src/com/shiv/solutions/leetcode/LongestSubstringInString.java
@@ -3,10 +3,19 @@ package com.shiv.solutions.leetcode;
 import java.util.HashMap;
 import java.util.Map;
 
-/*
- * https://leetcode.com/problems/longest-substring-without-repeating-characters/
+/**
+ * <pre>
+ * @problem
+ * <a href="https://leetcode.com/problems/longest-substring-without-repeating-characters/">Leetcode</a>
  * 
+ * @description
  * Find the length of the longest substring without repeating characters.
+ * Do not change the sequence of characters.
+ * 
+ * @explanations
+ * <a href="https://www.baeldung.com/java-longest-substring-without-repeated-characters">Baeldung</a>
+ * <a href="https://www.interviewbit.com/blog/longest-substring-without-repeating-characters/">InterviewBit</a>
+ * </pre>
  * 
  */
 public class LongestSubstringInString {
@@ -15,7 +24,7 @@ public class LongestSubstringInString {
 	    Map<Character, Integer> visited = new HashMap<>();
 	    String output = "";
 	    System.out.println("Input String: " + input);
-	    for (int start = 0, end = 0; end < input.length(); end++) {
+	    for (int start=0, end=0; end<input.length(); end++) {
 	        char currChar = input.charAt(end);
 	        if (visited.containsKey(currChar)) {
 	            start = Math.max(visited.get(currChar)+1, start);
